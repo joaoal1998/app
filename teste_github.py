54381642ff91c1ad4c4c66fb2606d537c9fd7c7e
João Paulo dos Santos <134109339+joaoal1998@users.noreply.github.com>
2024-06-13 17:04:02 -0300

    Update teste_github.py

--- teste_github.py
+++ teste_github.py
@@ -5,24 +5,19 @@ from io import StringIO
 
 if st.button("Buscar"):
     
-    # Seu token de acesso pessoal do GitHub
     token = st.secrets["database"]["token"]
-    
-    # URL do arquivo CSV no repositório privado
+
     url = st.secrets["database"]["url"]
     
-    # Cabeçalhos para a requisição autenticada
     headers = {
         'Authorization': f'token {token}',
         'Accept': 'application/vnd.github.v3.raw'
     }
-    
-    # Fazendo a requisição para obter o conteúdo do CSV
+
     response = requests.get(url, headers=headers)
-    
-    # Verificando se a requisição foi bem-sucedida
+
     if response.status_code == 200:
-        # Lendo o conteúdo do CSV diretamente com pandas
+        
         csv_content = response.content.decode('ISO-8859-1')
         data = StringIO(csv_content)
         df = pd.read_csv(data)
