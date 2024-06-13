9ab462013b80b9c7a5397c8001057937b846a8b2
João Paulo dos Santos <134109339+joaoal1998@users.noreply.github.com>
2024-06-13 16:08:16 -0300

    Update teste_github.py

--- teste_github.py
+++ teste_github.py
@@ -2,36 +2,32 @@ import streamlit as st
 import requests
 import pandas as pd
 from io import StringIO
-import time
 
-# Seu token de acesso pessoal do GitHub e URL do arquivo CSV no repositório privado
-token = st.secrets["database"]["token"]
-url = st.secrets["database"]["url"]
-
-# Cabeçalhos para a requisição autenticada
-headers = {
-    'Authorization': f'token {token}',
-    'Accept': 'application/vnd.github.v3.raw'
-}
-
-@st.experimental_memo
-def get_data(timestamp) -> pd.DataFrame:
+if st.button("Buscar"):
+    
+    # Seu token de acesso pessoal do GitHub
+    token = st.secrets["database"]["token"]
+    
+    # URL do arquivo CSV no repositório privado
+    url = st.secrets["database"]["url"]
+    
+    # Cabeçalhos para a requisição autenticada
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3.raw'
+    }
+    
     # Fazendo a requisição para obter o conteúdo do CSV
     response = requests.get(url, headers=headers)
     
     # Verificando se a requisição foi bem-sucedida
     if response.status_code == 200:
         # Lendo o conteúdo do CSV diretamente com pandas
-        csv_content = response.content.decode('ISO-8859-1')  # Ajuste o encoding conforme necessário
+        csv_content = response.content.decode('ISO-8859-1')
         data = StringIO(csv_content)
         df = pd.read_csv(data)
-        return df
-    else:
-        st.error(f"Falha ao obter o arquivo: {response.status_code}")
-        return pd.DataFrame()  # Retorna um DataFrame vazio em caso de erro
-
-if st.button("Buscar"):
-    timestamp = time.time()  # Usa o tempo atual para forçar a atualização do cache
-    df = get_data(timestamp)
-    if not df.empty:
         st.dataframe(df)
+    else:
+        print(f"Falha ao obter o arquivo: {response.status_code}")
+
+
