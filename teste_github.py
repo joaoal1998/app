c39b39737076ef9b7cc7c607847c762ec3b95f9e
João Paulo dos Santos <134109339+joaoal1998@users.noreply.github.com>
2024-06-13 15:17:02 -0300

    Update teste_github.py

--- teste_github.py
+++ teste_github.py
@@ -5,5 +5,5 @@ from io import StringIO
 
 if st.button("Buscar"):
     url = 'https://raw.githubusercontent.com/joaoal1998/app/main/dados_genericos.csv'
-    df = pd.read_csv(url)
+    df = pd.read_csv(url, encoding='ISO-8859-1')
     st.dataframe(df)
