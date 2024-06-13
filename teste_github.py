b80f4caf2b12cd52e986718c3cb48abc2a0108cf
João Paulo dos Santos <134109339+joaoal1998@users.noreply.github.com>
2024-06-13 15:36:57 -0300

    Update teste_github.py

--- teste_github.py
+++ teste_github.py
@@ -4,6 +4,30 @@ import pandas as pd
 from io import StringIO
 
 if st.button("Buscar"):
-    url = 'https://raw.githubusercontent.com/joaoal1998/app/main/dados_genericos.csv'
-    df = pd.read_csv(url, encoding='ISO-8859-1')
-    st.dataframe(df)
+    
+    # Seu token de acesso pessoal do GitHub
+    token = st.secrets["database"]["token"]
+    
+    # URL do arquivo CSV no repositório privado
+    url = st.secrets["database"]["url"]
+    
+    # Cabeçalhos para a requisição autenticada
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3.raw'
+    }
+    
+    # Fazendo a requisição para obter o conteúdo do CSV
+    response = requests.get(url, headers=headers)
+    
+    # Verificando se a requisição foi bem-sucedida
+    if response.status_code == 200:
+        # Lendo o conteúdo do CSV diretamente com pandas
+        csv_content = response.content.decode('ISO-8859-1')
+        data = StringIO(csv_content)
+        df = pd.read_csv(data)
+        st.dataframe(df)
+    else:
+        print(f"Falha ao obter o arquivo: {response.status_code}")
+
+
